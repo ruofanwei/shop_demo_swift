f40d77fa60f318f58e9d87547d5c4035475cd18f
ruofan <px732amgo81sp8@gmail.com>
2023-11-27 14:32:58 +0800

    Initial Commit

---
+++ ShopDemo/ShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDemoApp.swift
+//  ShopDemo
+//
+//  Created by 魏偌帆 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct ShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
